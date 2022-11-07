924f23a8b255aeb27738e8a2b5abff8681156ee5
Bart Wijnants <bart.wijnants@hootsuite.com>
2022-11-07 18:12:36 +0100

    Change name

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,12 @@ import Head from "next/head";
 const Home = () => (
   <div>
     <Head>
-      <title>Team</title>
-      <meta name="description" content="Team" />
+      <title>Provest</title>
+      <meta name="description" content="Provest" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
     <header>
-      <h1 className="text-3xl font-bold underline">Team</h1>
+      <h1 className="text-3xl font-bold underline">Provest</h1>
     </header>
   </div>
 );
