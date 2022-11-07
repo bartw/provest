b3be6e0a536f7eb230d395b1b3d557d942932655
Bart Wijnants <bart.wijnants@hootsuite.com>
2022-11-07 18:27:40 +0100

    add supabase

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
-import "../styles/globals.css";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider, Session } from "@supabase/auth-helpers-react";
 import type { AppProps } from "next/app";
+import { useState } from "react";
+import "../styles/globals.css";
 
-const App = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
+const App = ({
+  Component,
+  pageProps,
+}: AppProps<{ initialSession: Session }>) => {
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+
+  return (
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  );
+};
 
 export default App;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,34 @@
+import { Auth, ThemeSupa } from "@supabase/auth-ui-react";
+import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
 import Head from "next/head";
 
-const Home = () => (
-  <div>
-    <Head>
-      <title>Provest</title>
-      <meta name="description" content="Provest" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <header>
-      <h1 className="text-3xl font-bold underline">Provest</h1>
-    </header>
-  </div>
-);
+const Home = () => {
+  const session = useSession();
+  const supabase = useSupabaseClient();
+
+  return (
+    <div>
+      <Head>
+        <title>Provest</title>
+        <meta name="description" content="Provest" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header>
+        <h1 className="text-3xl font-bold underline">Provest</h1>
+      </header>
+      <main>
+        {session ? (
+          <p>Account page will go here.</p>
+        ) : (
+          <Auth
+            supabaseClient={supabase}
+            appearance={{ theme: ThemeSupa }}
+            theme="dark"
+          />
+        )}
+      </main>
+    </div>
+  );
+};
 
 export default Home;
